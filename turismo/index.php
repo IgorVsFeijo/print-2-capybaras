698b76c92c85c17e71d611054da1def8a8daada3
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-11-01 00:53:15 -0300

    teste 2 php

--- turismo/index.php
+++ turismo/index.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <link rel="stylesheet" href="../style/style.css">
+        <link rel="stylesheet" href="/style/style.css">
     <title>Home</title>
 </head>
 
@@ -15,7 +15,7 @@
     <!-- Inicio Header -->
 
     <?php
-    include '../includes/inc_header.php';
+    include '/includes/inc_header.php';
   ?>
 
 <!-- Fim Header -->
