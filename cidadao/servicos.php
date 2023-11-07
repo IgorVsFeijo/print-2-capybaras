f3674abf2cf162a1f798dcc4f051b85440732998
EmanueleCs <emanucrissilva@gmail.com>
2023-11-07 01:59:58 -0300

    Títulos Área Cidadão, etc.

--- cidadao/servicos.php
+++ cidadao/servicos.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>São Roque - Título da Página</title>
+    <title>Serviços</title>
 
     <?php
         include '../includes/inc_head_links.php';
@@ -22,7 +22,7 @@
 
     <div class="container">
         <main class="row text-center">
-            <h1> Concursos Públicos e Processos Seletivos </h1>
+            <h1> Cartela de Serviços</h1>
                 <p>Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>
 
             <article class="row">
