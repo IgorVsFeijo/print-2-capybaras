009f2b51754f86db83d7e9854c0103f521ab5464
joao-psousa <joao.paulo.mk@gmail.com>
2023-11-13 18:39:42 -0300

    ouvidoria codigo
    blabla

--- ouvidoria.php
+++ ouvidoria.php
@@ -22,7 +22,47 @@
     <!-- Fim Header -->
 
     <div class="container">
-      <main></main>
+      <main>
+        <div class="row">
+  <section class="col-lg-12 col-md-12 col-sm-12">
+    <img src="http://placekitten.com/800/250" alt="foto de gato">
+  </section>
+  <article class="col-lg-8 col-md-6 col-sm-12">
+      <h2>Atribuições da Ouvidoria</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada dapibus. Fusce ut tellus orci. Proin tincidunt dolor quis sagittis blandit. Donec rhoncus enim vitae nunc lacinia,at aliquet tellus convallis. In maximus quis enim auctor mollis. Duis pharetra mollis urna quissagittis. Ut sagittis cursus purus in blandit. Praesent vehicula rutrum est at consectetur. Nullam acsollicitudin turpis. Cras suscipit libero quis lectus tincidunt, nec tristique dui placerat. Phasellus at massa eget libero rhoncus pellentesque.</p>
+      <p>
+  </article>
+  <aside class="col-lg-4 col-md-6 col-sm-12">
+    <h2>Sua Voz nos Ajuda</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada dapibus. Fusce ut tellus orci. Proin tincidunt dolor quis sagittis blandit. Donec rhoncus enim vitae nunc lacinia,at aliquet tellus convallis. In maximus quis enim auctor mollis. Duis pharetra mollis urna quissagittis. Ut sagittis cursus purus in blandit. Praesent vehicula rutrum est at consectetur. Nullam acsollicitudin turpis. Cras suscipit libero quis lectus tincidunt, nec tristique dui placerat. Phasellus at massa eget libero rhoncus pellentesque.</p>
+    <p>
+  </aside>
+</div>
+
+
+
+<div class="row">
+  <section class="col-lg-12 col-md-12 col-sm-12">
+
+    <form action="/enviar" method="post">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome" required><br><br>
+
+      <label for="telefone">Telefone:</label>
+      <input type="tel" id="telefone" name="telefone" required><br><br>
+
+      <label for="email">E-mail:</label>
+      <input type="email" id="email" name="email" required><br><br>
+
+      <label for="sugestoes">Sugestões:</label><br>
+      <textarea id="sugestoes" name="sugestoes" rows="4" cols="50" required></textarea><br><br>
+
+      <input type="submit" value="Enviar">
+    </form>
+  </section>
+</div>
+</main>
     </div>
 
     <footer>
