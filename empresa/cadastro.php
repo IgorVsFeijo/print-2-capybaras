a84a86239294d1d12af89e04b9e6d9fb0803f4ea
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-11-13 19:50:31 -0300

    includes footer

--- empresa/cadastro.php
+++ empresa/cadastro.php
@@ -2,122 +2,85 @@
 <html lang="PT-BR">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>São Roque - Título da Página</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>São Roque - Título da Página</title>
 
-    <?php
-        include '../includes/inc_head_links.php';
-      ?>
+  <?php
+  include '../includes/inc_head_links.php';
+  ?>
 
 </head>
-<body>
-    <!-- Inicio Header -->
 
-      <?php
-        include '../includes/inc_header.php';
-      ?>
+<body class="pages-empresa">
+  <!-- Inicio Header -->
 
-    <!-- Fim Header -->
-    <h2>Infome Seus dados para cadastro</h2>
+  <?php
+  include '../includes/inc_header.php';
+  ?>
 
-    <form>
+  <!-- Fim Header -->
+  <h2>Infome Seus dados para cadastro</h2>
+
+  <form>
     <div class="row text-center">
-        <div class="my-3 col-lg-6 col-md-6 col-sm-12">
-            <label class="form-label">Lorem:</label>
-            <input class="form-control" type="text" name="Lorem"/>
-        </div>
+      <div class="my-3 col-lg-6 col-md-6 col-sm-12">
+        <label class="form-label">Lorem:</label>
+        <input class="form-control" type="text" name="Lorem" />
+      </div>
 
-        <div class="my-3 col-lg-6 col-md-6 col-sm-12">
-            <label class="form-label">Lorem:</label>
-            <input class="form-control" type="text" name="Lorem"/>
-        </div>
+      <div class="my-3 col-lg-6 col-md-6 col-sm-12">
+        <label class="form-label">Lorem:</label>
+        <input class="form-control" type="text" name="Lorem" />
+      </div>
 
     </div>
 
     <div class="row text-center">
-        <div class="my-3 col-lg-6 col-md-6 col-sm-12">
-            <label class="form-label">Lorem:</label>
-            <input class="form-control" type="text" name="Lorem"/>
-        </div>
+      <div class="my-3 col-lg-6 col-md-6 col-sm-12">
+        <label class="form-label">Lorem:</label>
+        <input class="form-control" type="text" name="Lorem" />
+      </div>
 
-        <div class="my-3 col-lg-6 col-md-6 col-sm-12">
-            <label class="form-label">Lorem:</label>
-            <input class="form-control" type="text" name="Lorem"/>
-        </div>
-
-    </div>
-
-    <div class="row text-center">
-
-        <div class="my-3">
-            <label class="form-label">Lorem:</label>
-            <input class="form-control" type="text" name="Lorem"/>
-        </div>
+      <div class="my-3 col-lg-6 col-md-6 col-sm-12">
+        <label class="form-label">Lorem:</label>
+        <input class="form-control" type="text" name="Lorem" />
+      </div>
 
     </div>
 
     <div class="row text-center">
 
-        <div class="col-lg-12 col-md-12 col-sm-12">
+      <div class="my-3">
+        <label class="form-label">Lorem:</label>
+        <input class="form-control" type="text" name="Lorem" />
+      </div>
+
+    </div>
+
+    <div class="row text-center">
+
+      <div class="col-lg-12 col-md-12 col-sm-12">
         <input type="submit" value="Enviar">
-        </div>
-
-    </div>
-
-    </form>
-   <!-- Inicio do Footer -->
-
-  <footer>
-    <div class="container-lg">
-      <div class="d-flex align-items-center">
-        <figure>
-          <picture><img src="https://placekitten.com/200/150" alt=""></picture>
-        </figure>
-        <address class="flex-grow-1">
-          Av. São Paulo, Taboão, 966
-        </address>
-        <div class="desenvolvido-por">
-          <p>Desenvolvido pelas Carnívoras Capivaras da Justiça</p>
-          <div>
-            <a href="https://www.facebook.com/prefeituradesaoroque/?locale=pt_BR" target="_blank">
-              <i class="bi bi-facebook btn"></i>
-            </a>
-            <a href="https://www.instagram.com/prefeituradesaoroque/" target="_blank">
-              <i class="bi bi-instagram btn"></i>
-            </a>
-          </div>
-          <small>Todos os direitos reservados.</small>
-        </div>
       </div>
+
     </div>
-    <div class="text-center p-2 w-100 barra-acess" id="barra-acessibilidade2">
-      <div class="btn-group acess row justify-content-center">
-        <button type="button" class="btn btn-acessibilidade col-1">
-          <img src="https://placekitten.com/30/30" alt="place kitten maneiro" />
-        </button>
-        <button type="button" class="btn btn-acessibilidade col-1">
-          <img src="https://placekitten.com/30/30" alt="place kitten maneiro" />
-        </button>
-        <button type="button" class="btn btn-acessibilidade col-1">
-          <img src="https://placekitten.com/30/30" alt="place kitten maneiro" />
-        </button>
-        <button type="button" class="btn btn-acessibilidade col-1">
-          <img src="https://placekitten.com/30/30" alt="place kitten maneiro" />
-        </button>
-        <button type="button" class="btn btn-acessibilidade col-1">
-          <img src="https://placekitten.com/30/30" alt="place kitten maneiro" />
-        </button>
-      </div>
-    </div>
-  </footer>
+
+  </form>
+  <!-- Inicio do Footer -->
+
+    <?php
+      include '../includes/inc_footer.php';
+    ?>
 
   <!-- Fim do Footer  -->
 
-    <?php
-      include '../includes/inc_bootstrap_js.php';
-    ?>
+  <?php
+  include '../includes/inc_bootstrap_js.php';
+  ?>
 </body>
+
 </html>
 </body>
+
 </html>
