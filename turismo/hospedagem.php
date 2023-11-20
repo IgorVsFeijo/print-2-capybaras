a27251e195a3ddc44bf1e30c6469502986ad9ff8
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2023-11-20 15:45:13 -0300

    Atualização Hospedagem coisa linda

--- turismo/hospedagem.php
+++ turismo/hospedagem.php
@@ -21,143 +21,133 @@
     <!-- Fim Header -->
     <div class="container text-center">
         <div class="row justify-content-start">
-            <p class="col-2">lorem</p>
+            <p class="col-5 text-start">Home > Hospedagem</p>
         </div>
 
         <article>
-            <h1>h1 - Article</h1>
+            <h2>Acampamentos</h2>
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Sitio de Lazer</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/acamp_sdl_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Estrada Turística do Morro do Saboó, Km 0</p>
+                    <p>Descrição: Um delicioso lugar para passar seu final de semana, com muitas rotinas de lazer.
                     </p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Acampamento</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/sem_emp_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Indefinido</p>
+                    <p>Descrição: Para divulgar sua empresa no site de turismo de São Roque, é necessário fazer um pequeno cadastro.
                     </p>
                 </section>
             </div>
         </article>
 
         <article>
-            <h1>h1 - Article</h1>
+            <h2>Hotéis</h2>
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Hotel</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/sem_emp_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Indefinido</p>
+                    <p>Descrição: Para divulgar sua empresa no site de turismo de São Roque, é necessário fazer um pequeno cadastro.
                     </p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Cordialle Hotel</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/hot_cordialle_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Rua Sotero de Souza, 500, Centro - São Roque</p>
+                    <p>Descrição: Uma interpretação contemporânea das tradicionais hospedarias. Aqui, as tradições da hospitalidade são mantidas com personalidade, refletindo-se em nossa maneira única de receber e interagir com nossos hóspedes.
                     </p>
                 </section>
             </div>
 
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>São Roque Park Hotel</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/hot_park_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Avenida Antônino Dias Bastos, 318 - Centro</p>
+                    <p>Descrição: Liberte-se do estresse ao escolher o São Roque Park Hotel, onde conforto e qualidade se unem a uma ótima relação custo-benefício. Reserve um momento para relaxar com sua família.
                     </p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Villa Rossa</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/hot_villa_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Rua Cora Coralina, 350, Vila Darcy Penteado</p>
+                    <p>Descrição: Experimente a plenitude da natureza em mais de 350.000m2 cercados pela exuberante Mata Atlântica. Inspire o ar puro da floresta ao percorrer nossas alamedas, embalado pelo suave canto dos pássaros.
                     </p>
                 </section>
             </div>
         </article>
 
         <article>
-            <h1>h1 - Article</h1>
+            <h2>Pousadas</h2>
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Aconchego no Campo</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/pou_campo_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Rua Cora Coralina, 350, Vila Darcy Penteado</p>
+                    <p>Descrição: Um aconchegante e romântico lugar no campo.
                     </p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Pousada Juriti</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/pou_juriti_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Estrada da Fonte, 1352 – São Roque – SP</p>
+                    <p>Descrição: Se você busca serenidade, simplicidade e conforto para você e sua família, a Pousada Juriti é uma excelente escolha. Com mais de 25 anos de experiência no setor hoteleiro, atendemos hóspedes que desejam explorar São Roque, a famosa "Terra do Vinho". Venha desfrutar de momentos especiais com sua família!
                     </p>
                 </section>
             </div>
 
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Chácara Putini</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/pou_putini_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Rodovia Raposo Tavares, km 55,5 - Mailasqui</p>
+                    <p>Descrição: As instalações incluem lagos, quiosques, churrasqueiras, piscinas, galeria de arte e quadra poliesportiva, tudo harmoniosamente integrado à natureza, proporcionando o conforto de suítes, apartamentos e encantadores bangalôs totalmente equipados. Viva essa experiência dos sonhos!
                     </p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/417/255" alt="Imagem Via Placeholder">
+                    <p>Sítio da Terra e Arte</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/pou_terra_desktop.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Local</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit nisi id repellat magnam iure
-                        veniam possimus debitis ea est totam et labore, ratione nobis minus nihil rem consectetur ad.
+                    <p>LOCAL: Estr. Brasília - Sitio da Terra e Arte, São Roque</p>
+                    <p>Descrição: Em meio a mais de 50 mil metros quadrados de natureza deslumbrante, descubra trilhas, 350 metros de rio, pássaros, flores, árvores centenárias, ovelhas, marrecos, a cativante mula Rebeca, galos exóticos, uma granja de ovos caipira e uma vista que enche os olhos e os pulmões com o puro ar da montanha.
                     </p>
                 </section>
             </div>
