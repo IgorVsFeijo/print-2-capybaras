591d768ebc7f59013ca67ad2756e6ddb78b77012
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2023-11-20 15:09:57 -0300

    Imagens e bootstrap organizados index

--- turismo/index.php
+++ turismo/index.php
@@ -22,133 +22,121 @@
     <div class="container text-center">
         <div class="row">
             <figure>
-                <img src="http://placekitten.com/1440/300" alt="Imagem Via Placeholder">
+                <img src="../imgs/turismo/topo_desktop.jpg" alt="Imagem referente a brasital com o logo de são roque">
             </figure>
         </div>
 
         <article>
-            <div class="row justify-content-start">
-                <h1 class="col-2">h1 - Article</h1>
+            <div class="row text-start">
+                <h2 class="col-2">EVENTOS</h2>
             </div>
             <div class="row">
                 <figure class="col-lg-6 col-md-6 col-sm-12">
-                    <img src="http://placekitten.com/473/284" alt="Imagem Via Placeholder">
+                    <img src="../imgs/turismo/evento_castra.jpg" alt="Imagem do evento castra movel, com a data 17 de setembro no local chamado recanto">
                 </figure>
-                <section class="col-lg-6 col-md-6 col-sm-12">
-                    <h3>h3 - Section</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam natus magni incidunt eaque ut
-                        dolor impedit quod neque, vero fuga voluptatibus modi architecto ad tempore autem enim adipisci
-                        alias unde! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error fuga quos eligendi
-                        beatae quod deleniti quia non accusamus cupiditate. Voluptatibus quibusdam minus culpa vero
-                        necessitatibus et modi ut fugit quo?</p>
+                <section class="col-lg-6 col-md-6 col-sm-12 text-start">
+                    <h3>Castra Móvel</h3>
+                    <p>O Castramóvel está a caminho de uma nova parada em São Roque, desta vez na região do Junqueira. No domingo, dia 17 de setembro, o serviço que oferece castração gratuita para cães e gatos estará disponível no Recanto da Cascata, proporcionando acolhimento e cuidado aos pets da população.</p>
+                    <p>Os interessados em castrar seus animais devem se inscrever até o dia 16 de setembro (sábado) através do site da prefeitura ou presencialmente até esta sexta-feira (15), no Centro de Saúde II, localizado na Ladeira Alfredo Salveti, 129 - Centro, durante o horário de atendimento da unidade.</p>
                 </section>
             </div>
         </article>
 
         <aside>
             <div class="row justify-content-start">
-                <h2 class="col-2">h2 - Aside</h2>
+                <h2 class="col-2">HOSPEDAGEM</h2>
             </div>
-            <div class="row">
+            <div class="row text-start">
                 <section class="col-lg-4 col-md-4 col-sm-6">
-                    <h3>h3 - Section</h3>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h3>Sitio de Lazer</h3>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/sitio_de_lazer.jpg" alt="Imagem referente a logo do acampamento sitio de lazer">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laborum, tempora sit labore cum
-                        consequatur autem minima cumque?.</p>
+                    <p>Um delicioso lugar para passar seu final de semana, com muitas rotinas de lazer.</p>
                 </section>
                 <section class="col-lg-4 col-md-4 col-sm-6">
-                    <h3>h3 - Section</h3>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h3>Cordialle Hotel</h3>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/cordialle.jpg" alt="Imagem referente a logo do hotel cordialle.">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laborum, tempora sit labore cum
-                        consequatur autem minima cumque?.</p>
+                    <p>Uma interpretação contemporânea das tradicionais hospedarias.</p>
                 </section>
                 <section class="col-lg-4 col-md-4 col-sm-6">
-                    <h3>h3 - Section</h3>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h3>Chácara Putini</h3>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/putini.jpg" alt="Imagem referente a logo da chácara putini.">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laborum, tempora sit labore cum
-                        consequatur autem minima cumque?.</p>
+                    <p>As instalações incluem lagos, quiosques, churrasqueiras, piscinas, galeria de arte e quadra poliesportiva.</p>
                 </section>
             </div>
         </aside>
 
-
         <aside>
             <div class="row justify-content-start">
-                <h3 class="col-2">h3 - Aside</h3>
+                <h3 class="col-2">GALERIA</h3>
             </div>
             <div class="row justify-content-end">
                 <a class="col-2" href="#">ver mais+</a>
             </div>
             <div class="row">
-                <figure class="col-lg-6 col-md-6 col-sm-6">
-                    <img src="http://placekitten.com/473/284" alt="Imagem Via Placeholder">
+                <figure class="col-lg-6 col-md-6 col-sm-6 text-center">
+                    <img src="../imgs/turismo/brasital_galeria.jpg" alt="Imagem referente ao local turistico chamado brasital">
                 </figure>
-                <figure class="col-lg-6 col-md-6 col-sm-6">
-                    <img src="http://placekitten.com/473/284" alt="Imagem Via Placeholder">
+                <figure class="col-lg-6 col-md-6 col-sm-6 text-center">
+                    <img src="../imgs/turismo/ferro_galeria.jpg" alt="Imagem referente a ferroviaria turistica de são roque">
                 </figure>
             </div>
         </aside>
 
         <aside>
             <div class="row justify-content-start">
-                <h3 class="col-2"> h3 - Aside</h3>
+                <h3 class="col-5 text-start"> PONTOS TURISTICOS</h3>
             </div>
-            <div class="row">
+            <div class="row text-start">
                 <section class="col-lg-6 col-md-6 col-sm-12">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/473/255" alt="Imagem Via Placeholder">
+                    <p>Praça da republica</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/praça_republica.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odio recusandae aliquam
-                        incidunt suscipit accusamus dolores fuga!</p>
+                    <p>Conhecida como “Praça da Preguiça”, por ter recebido a visita de um bicho preguiça da região.</p>
                 </section>
                 <section class="col-lg-6 col-md-6 col-sm-12">
                   <h3 class="hide">h3 - Section</h3>
-                    <p>lorem</p>
-                    <figure>
-                        <img src="http://placekitten.com/473/255" alt="Imagem Via Placeholder">
+                    <p>Outlet Catarina</p>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/outlet_catarina.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odio recusandae aliquam
-                        incidunt suscipit accusamus dolores fuga!</p>
+                    <p>Um dos melhores pontos de São Roque para fazer umas compras, sair em um final de semana com a família.</p>
                 </section>
             </div>
         </aside>
 
         <section>
             <div class="row justify-content-start">
-                <h3 class="col-2">h3 - Section</h3>
+                <h3 class="col-2">GASTRONOMIA</h3>
             </div>
-            <div class="row">
+            <div class="row text-start">
                 <section class="col-lg-4 col-md-4 col-sm-12">
-                    <h4>h4 - Section</h4>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h4>Vinhos Alma Galiza</h4>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/alma_galiza.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odio recusandae aliquam
-                        incidunt suscipit accusamus dolores fuga!</p>
+                    <p>Vinhos Alma Galiza conta com uma porção dos melhores vinhos da região, e uma gastronomia portuguesa de tirar fôlego.</p>
                 </section>
                 <section class="col-lg-4 col-md-4 col-sm-12">
-                    <h4>h4 - Section</h4>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h4>Come Together</h4>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/cct.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odio recusandae aliquam
-                        incidunt suscipit accusamus dolores fuga!</p>
+                    <p>Desde 2015, um porão de um casarão centenário de São Roque/SP abriga um clássico pub com bons chopes, cocktails, burgers, porções e muita diversão.</p>
                 </section>
                 <section class="col-lg-4 col-md-4 col-sm-12">
-                    <h4>h4 - Section</h4>
-                    <figure>
-                        <img src="http://placekitten.com/270/255" alt="Imagem Via Placeholder">
+                    <h4>Tia Lina</h4>
+                    <figure class="text-center">
+                        <img src="../imgs/turismo/tia_lina.jpg" alt="Imagem Via Placeholder">
                     </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odio recusandae aliquam
-                        incidunt suscipit accusamus dolores fuga!</p>
+                    <p>Você e sua família podem desfrutar de um lugar descontraído, com muito lazer e atividades, admirar belas paisagens e saborear o melhor da culinária italiana.</p>
                 </section>
             </div>
         </section>
