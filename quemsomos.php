d701692b4634b1f92fd8cdd77e28cbc57101aa0b
Henrique Silva Ribeiro <henribeirox@gmail.com>
2023-11-22 13:08:12 -0300

    Update quemsomos.php
    Manu, mexi sem querer de novo, desculpaaaaa mas já arrumei

--- quemsomos.php
+++ quemsomos.php
@@ -9,11 +9,8 @@
     <?php
         include 'includes/inc_head_links.php';
       ?>
-<<<<<<< Updated upstream
       <link rel="stylesheet" href="/style/cultura.css">
-=======
       <link rel="stylesheet" href="/style/cidadao.css">
->>>>>>> Stashed changes
 
 </head>
 
@@ -30,116 +27,25 @@
       <main class="row quemsomos">
         <h1 class="col-12 text-center">Sobre a equipe</h1>
         <p class="col-lg-6">
-<<<<<<< Updated upstream
-=======
           Somos um grupo de amigos que se complementa e busca sempre fazer o melhor que pudermos. Nos conhecemos na Fatec São Roque e esse site é o resultado de um trabalho de três meses voltado às matérias Práticas de Projeto de Sítio para Internet e Prática de Design. 
         </p>
         <p class="col-lg-6">
           O tema do site é o portal de uma cidade, sendo assim escolhemos a cidade em que nossa faculdade é sediada, São Roque. Agradecemos por terem chego até aqui e esperamos que tenham desfrutado do nosso site. Conheça a dedicada equipe por trás deste projeto.
         </p>
-
->>>>>>> Stashed changes
+        
         <article>
           <div class="row align-items-center sombra">
             <figure class="col-lg-4 col-md-4 text-center">
               <img src="https://placekitten.com/200/200" alt="" />
             </figure>
-<<<<<<< Updated upstream
-            <div class="col-lg-8 col-md-8">
-              <h2>Emanuele Cristina da Silva</h2>
-              <p>R.A.: 2650832313017</p>
-              <p>
-              Olá! Meu nome é Emanuele, tenho 20 años. Sempre tive interesse em entender como as coisas funcionam, então aqui estou. Aproveite nosso site!
-              </p>
-              <p>Função:</p>
-              <p>Área: <a href="../print-2-capybaras/cidadao/index.php">Cidadão</a></p>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div class="row">
-            <figure class="col-lg-4 col-md-4">
-              <img src="https://placekitten.com/200/200" alt="" />
-            </figure>
-            <div class="col-lg-8 col-md-8">
-              <h2>Henrique Silva Ribeiro</h2>
-              <p>R.A.: 2650832313014</p>
-              <p>
-              Opa, e aí? Sou o Henrique, tenho 18 anos e moro nessa cidadezinha legal de São Roque. Sou uma pessoa tranquila, um pouco preguiçosa, mas te garanto que dei meu melhor para a construção desse projeto para você. Espero que goste de sua passagem por ele.
-              </p>
-              <p>Função:</p>
-              <p>Área: <a href="../print-2-capybaras/cultura/index.php">Cultura</a></p>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div class="row">
-            <figure class="col-lg-4 col-md-4">
-              <img src="https://placekitten.com/200/200" alt="" />
-            </figure>
-            <div class="col-lg-8 col-md-8">
-              <h2>Igor Vinicius Silveira Feijó</h2>
-              <p>R.A.: 2650832313019</p>
-              <p>
-              Oi, sou Igor. Na Fatec, estudo Desenvolvimento Web de Sites e Apps. Aprendendo a programar em PHP. Espero que goste do site, foi feito com amor!
-              </p>
-              <p>Função:</p>
-              <p>Área: <a href="../print-2-capybaras/cidade/index.php">Cidade</a></p>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div class="row">
-            <figure class="col-lg-4 col-md-4">
-              <img src="https://placekitten.com/200/200" alt="" />
-            </figure>
-            <div class="col-lg-8 col-md-8">
-              <h2>João Paulo de Sousa</h2>
-              <p>R.A.: 2650832313007</p>
-              <p>
-              Eu sou o João, tenho 29 anos, sou de Curitiba e estou aqui tentando uma vida "melhor" na segunda FATEC. Boa visualização do nosso site.
-              </p>
-              <p>Função:</p>
-              <p>Área: <a href="../print-2-capybaras/governo/index.php">Governo</a></p>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div class="row">
-            <figure class="col-lg-4 col-md-4">
-              <img src="https://placekitten.com/200/200" alt="" />
-            </figure>
-            <div class="col-lg-8 col-md-8">
-              <h2>Leonardo Marques</h2>
-              <p>R.A.: 2650832313012</p>
-              <p>
-              Oi! Eu sou o Leonardo, estudante da Fatec São Roque, sou apaixonado por jogos e estou aprendendo php, espero que gostem do site!
-              </p>
-              <p>Função: Designer</p>
-              <p>Área: <a href="../print-2-capybaras/empresa/home.php">Empresa</a></p>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div class="row">
-            <figure class="col-lg-4 col-md-4">
-              <img src="https://placekitten.com/200/200" alt="" />
-            </figure>
-            <div class="col-lg-8 col-md-8">
-=======
             <div class="col-lg-8 col-md-8 quemsomos_conteudo align-items-center">
->>>>>>> Stashed changes
               <h2>Pietro Ricardo Black Fracalanza Muzy</h2>
               <p><b>R.A.:</b> 2650832313027</p>
               <p>
               Olá, nobre visitante do nosso site. Eu me chamo Pietro Ricardo, tenho 18 anos e sou uma pessoa hiperativa, apaixonado pelo mundo virtual. Até!!!
               </p>
-<<<<<<< Updated upstream
               <p>Função: Editor de fotos</p>
               <p>Área: <a href="../print-2-capybaras/turismo/index.php">Turismo</a></p>
-=======
-              <p><b>Função:</b> Designer</p>
-              <p><b>Área:</b> <a href="/turismo/index.php">Turismo</a></p>
             </div>
           </div>
         </article>
@@ -225,7 +131,6 @@
               </p>
               <p><b>Função:</b> Editor de imagens</p>
               <p><b>Área:</b> <a href="/empresa/home.php">Empresa</a></p>
->>>>>>> Stashed changes
             </div>
           </div>
         </article>
