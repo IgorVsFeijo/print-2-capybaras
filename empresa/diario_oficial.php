f1cf2e3b9ff9c4acb01e4e39a3aa959d4b684fb4
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2023-11-26 15:01:34 -0300

    Diario Oficial COMPLETO!

--- empresa/diario_oficial.php
+++ empresa/diario_oficial.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>São Roque - Título da Página</title>
+  <title>São Roque - Diário Oficial</title>
 
   <?php
   include '../includes/inc_head_links.php';
@@ -27,130 +27,138 @@
         </div>
       <h2>Edição Atual</h2>
         <section class="edicao">
-          <div class="row col-12">
+          <div class="row text-center">
             <h3 class="hide">H3- Section</h3>
-            <div class="col-6">
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario.jpg" alt="Imagem de uma folha de papel tamanho A4 escrito sumario">
               </figure>
-              <div class="col-4">
-                <h2>Lorem Ipsum</h2><br>
-                <h2>Lorem Ipsum</h2>
-                <h2>Lorem Ipsum</h2>
-                <h2>Lorem Ipsum</h2>
-                <h2>Lorem Ipsum</h2>
-              </div>
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 7</h2><br>
+                <p><b>Postagem:</b> 24/11/2023 às 16h23</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 4,31 MB | 49 páginas<p>
+                <p><b>Visualizações:</b> 301</p>
             </div>
-            <div class="row col-2">
-              <a class="btn btn-vermais botoes_diario col-6" href="#">Ler</a>
-              <a class="btn btn-vermais botoes_diario col-6" href="#">Baixar</a>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
             </div>
           </div>
         </section>
         
         <hr>
 
-        <div class="passadas">
+        <div>
           <h2>Edições Passadas</h2>
           <section class="row edicao">
-            <div class="col-12">
-              <h3 class="hide">H3- Section</h3>
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+          <div class="row text-center">
+            <h3 class="hide">H3- Section</h3>
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center ">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario_2.jpg" alt="Imagem de 3 folhas de papel tamanho A4 escrito sumario">
               </figure>
-              <h2>Lorem Ipsum</h2><br>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-
-              <div class="row col-4">
-                <button class="btn btn-primary">Ler</button>
-                <button class="btn btn-primary">Baixar</button>
-              </div>
-
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 6</h2><br>
+                <p><b>Postagem:</b> 22/11/2023 às 16h15</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 3,83 MB | 51 páginas</p>
+                <p><b>Visualizações:</b> 377</p>
             </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
+            </div>
+          </div>
           </section>
 
           <section class="row edicao">
-            <div class="col-12">
-              <h3 class="hide">H3- Section</h3>
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+          <div class="row text-center">
+            <h3 class="hide">H3- Section</h3>
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario_2.jpg" alt="Imagem de 3 folhas de papel tamanho A4 escrito sumario">
               </figure>
-              <h2>Lorem Ipsum</h2><br>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-
-              <div class="row col-4">
-                <button class="btn btn-primary">Ler</button>
-                <button class="btn btn-primary">Baixar</button>
-              </div>
-
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 5</h2><br>
+                <p><b>Postagem:</b> 17/11/2023 às 16h15</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 1,19 MB | 19 páginas</p>
+                <p><b>Visualizações:</b> 276</p>
             </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
+            </div>
+          </div>
           </section>
 
           <section class="row edicao">
-            <div class="col-12">
-              <h3 class="hide">H3- Section</h3>
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+          <div class="row text-center">
+            <h3 class="hide">H3- Section</h3>
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario_2.jpg" alt="Imagem de 3 folhas de papel tamanho A4 escrito sumario">
               </figure>
-              <h2>Lorem Ipsum</h2><br>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-
-              <div class="row col-4">
-                <button class="btn btn-primary">Ler</button>
-                <button class="btn btn-primary">Baixar</button>
-              </div>
-
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 4</h2><br>
+                <p><b>Postagem:</b> 14/11/2023 às 16h00</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 3,69 MB | 37 páginas</p>
+                <p><b>Visualizações:</b> 542</p>
             </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
+            </div>
+          </div>
           </section>
 
           <section class="row edicao">
-            <div class="col-12">
-              <h3 class="hide">H3- Section</h3>
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+          <div class="row text-center">
+            <h3 class="hide">H3- Section</h3>
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario_2.jpg" alt="Imagem de 3 folhas de papel tamanho A4 escrito sumario">
               </figure>
-              <h2>Lorem Ipsum</h2><br>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-
-              <div class="row col-4">
-                <button class="btn btn-primary">Ler</button>
-                <button class="btn btn-primary">Baixar</button>
-              </div>
-
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 3</h2><br>
+                <p><b>Postagem:</b> 11/11/2023 às 16h45</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 3,33 MB | 32 páginas</p>
+                <p><b>Visualizações:</b> 156</p>
             </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn btn-vermais botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
+            </div>
+          </div>
           </section>
 
           <section class="row edicao">
-            <div class="col-12">
-              <h3 class="hide">H3- Section</h3>
-              <figure>
-                <img src="https://placekitten.com/200/300" alt="gatinho">
+          <div class="row text-center">
+            <h3 class="hide">H3- Section</h3>
+              <figure class="col-lg-3 col-md-3 col-sm-3 text-center ">
+                <img class="img_diario" src="../imgs/empresa/emp_sumario_2.jpg" alt="Imagem de 3 folhas de papel tamanho A4 escrito sumario">
               </figure>
-              <h2>Lorem Ipsum</h2><br>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-              <h2>Lorem Ipsum</h2>
-
-              <div class="row col-4">
-                <button class="btn btn-primary">Ler</button>
-                <button class="btn btn-primary">Baixar</button>
-              </div>
-
+              <div class="col-lg-4 col-md-4 col-sm-3 text-start emp_textos">
+                <h2>Edição Nº 2</h2><br>
+                <p><b>Postagem:</b> 07/11/2023 às 16h30</p>
+                <p><b>Titular:</b> MUNICIPIO DE SAO ROQUE</p>
+                <p><b>Tamanho:</b> 4,64 MB | 49 páginas</p>
+                <p><b>Visualizações:</b> 609</p>
             </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 text-center emp_position_a">
+              <a class="btn botoes_diario" href="#"><i class="bi bi-book"></i> Ler</a>
+              <a class="btn botoes_diario" href="#"><i class="bi bi-download"></i> Baixar</a>
+            </div>
+          </div>
           </section>
+
+          <nav class ="emp_page_link">
+          <ul class="pagination">
+            <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item disabled"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          </ul>
+</nav>
   </div>
   
 
