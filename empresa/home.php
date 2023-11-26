9dce80eb24a79cd572b424ab028bcf477dda0d28
Henrique Silva Ribeiro <henribeirox@gmail.com>
2023-11-26 15:22:34 -0300

    Update home.php

--- empresa/home.php
+++ empresa/home.php
@@ -30,12 +30,13 @@
 
       <div class="row text-center">
         <figure class="col-lg-4 col-md-6 col-sm-12">
-            <img src="https://placekitten.com/300/300" alt="gatinho">
+            <img src="https://placekitten.com/300/200" alt="gatinho">
         </figure>
         <aside class="col-lg-8 col-md-6 col-sm-12">
             <article>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum neque non gravida egestas. In aliquam mauris non placerat hendrerit. Morbi dictum, mauris et feugiat rhoncus, urna enim bibendum magna, non posuere leo odio id libero. Nunc luctus urna ante, id maximus nisl sagittis laoreet. Morbi in nisi faucibus, iaculis felis nec, mattis nisl. Vivamus arcu augue, vestibulum et placerat et, mattis eget massa. Morbi a felis nec felis vestibulum suscipit id at diam. Aliquam pellentesque mauris in hendrerit mollis. Praesent tempus varius erat ac porttitor. Duis ornare ex nec pulvinar euismod. Nulla convallis ex metus, eget consectetur nulla lacinia in. </p>
+                <h2>IPTU</h2>
+                <p>Veja tudo que precisa saber para manter a sua empresa com o IPTU em dia, como cadastrar, como manter atualizado, como transferir o cadastro da IPTU, isenções e possíveis descontos, além dos locais em que você pode acessar essas informações, bem como os documentos necessários para o cadastro do endereço de correspondência.</p>
+                <a class="btn btn-vermais botao_empresa col-2" href="../empresa/diario_oficial.php">Ver mais</a>
             </article>
         </aside>
       </div>
@@ -44,28 +45,24 @@
         <h3 class="hide">H3- Section</h3>
         <div class="col-4">
           <figure>
-              <img src="https://placekitten.com/200/300" alt="gatinho">
+              <img src="https://placekitten.com/300/150" alt="gatinho">
           </figure>
         </div>
         <div class="col-4">
-          <h2>Lorem Ipsum</h2>
-          <h2>Lorem Ipsum</h2>
-          <h2>Lorem Ipsum</h2>
-          <h2>Lorem Ipsum</h2>
-          <h2>Lorem Ipsum</h2>
+          <h2>DIÁRIO OFICIAL 2023</h2>
+          <p>Confira as últimas atualizações do Diário Oficial!</p>
         </div>
-          <a class="btn btn-vermais botao_empresa col-2" href="#">Ver mais</a>
+          <a class="btn btn-vermais botao_empresa col-2" href="../empresa/diario_oficial.php">Ver mais</a>
       </section>
 
       <hr>
 
       <section class="row text-center col-12">
+      <h3 class="hide">H3- Section</h3>
         <div class="col-sm-12 col-md-6">
-            <h3 class="hide">H3- Section</h3>
             <a href="../empresa/cadastro.php" class="link_home">Cadastro de Pessoa Juridica</a>
         </div>
         <div class="col-sm-12 col-md-6">
-            <h3 class="hide">H3- Section</h3>
             <a href="../empresa/aprovacao.php" class="link_home">Aprovação de Projetos</a>
         </div>
       </section>
