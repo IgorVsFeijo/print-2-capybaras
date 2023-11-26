3bcf22be7f50725e61c119fc08e0c2f7e1beee36
EmanueleCs <emanucrissilva@gmail.com>
2023-11-26 20:18:17 -0300

    Titles Empresa

--- empresa/cadastro.php
+++ empresa/cadastro.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>São Roque - Título da Página</title>
+  <title>São Roque - Cadastro</title>
 
   <?php
   include '../includes/inc_head_links.php';
