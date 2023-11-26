2e0c395e619711b8a324558aab29bb7dcb7c1f49
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2023-11-25 22:49:18 -0300

    noticias terminadas

--- noticia2.php
+++ noticia2.php
@@ -32,7 +32,7 @@
             </div>
             <h3>Cidade</h3>
                 <figure class="text-center">
-                    <img src="http://placekitten.com/760/349" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_agro_2.webp" alt="Imagem referente a noticia do festival encontro ecologico">
                 </figure>
                 <p><b>Ministro Paulo Teixeira visita São Roque em encontro agroecológico</b></p>
                 <p>Na última semana, o Ministro do Desenvolvimento Agrário e Agricultura Familiar do Brasil, Paulo Teixeira, esteve em São Roque para participar da mais recente edição do Encontro Paulista de Agroecologia. Essa iniciativa, promovida pela Articulação Paulista de Agroecologia em parceria com a Prefeitura local, o Instituto Federal e o Governo Federal, introduziu um evento inédito no município. O encontro visou promover a interação, troca de conhecimentos, celebração e articulação em torno dos desafios e potencialidades da agroecologia.</p>
@@ -41,41 +41,41 @@
                 <p>Ao término do encontro, a organização realizou a doação de aproximadamente 100kg de alimentos para a Casa de Passagem de São Roque. Esse evento posicionou São Roque no centro de um movimento que está ganhando cada vez mais força, não apenas na região, mas em todo o país e no cenário internacional.</p>
             </article>
 
-            <section class="col-lg-4 col-md-4 col-sm-12 not_vejamais">
+            <section class="col-lg-3 col-md-3 col-sm-12 not_vejamais">
                 <h4>VEJA MAIS</h4>
                 <figure>
-                    <img src="http://placekitten.com/294/176" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_cancer_2.webp" alt="Imagem referente a noticia sobre a prevenção do cancer de prostata">
                 </figure>
-                <a class="not_link" href="#">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a class="not_link text-start" href="#">
+                    <p>Prefeitura promove palestra de Conscientização e Prevenção do Câncer de Próstata</p>
                 </a>
 
                 <figure>
-                    <img src="http://placekitten.com/294/176" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_jose_2.webp" alt="Imagem referente a noticia do festival Jose cabinda">
                 </figure>
-                <a class="not_link" href="#">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a class="not_link text-start" href="#">
+                    <p>Festival José Cabinda acontece neste domingo e segunda-feira</p>
                 </a>
 
                 <figure>
-                    <img src="http://placekitten.com/294/176" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_imoveis_2.webp" alt="Imagem referente a noticia da abertura da anistia para regularização de imóveis">
                 </figure>
-                <a class="not_link" href="#">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a class="not_link text-start" href="#">
+                    <p>Prefeitura abre anistia para regularização de imóveis</p>
                 </a>
 
                 <figure>
-                    <img src="http://placekitten.com/294/176" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_rotatoria_2.webp" alt="Imagem referente a noticia da construção da rotatoria fixa">
                 </figure>
-                <a class="not_link" href="#">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a class="not_link text-start" href="#">
+                    <p>Rotatória fixa será construída na região próxima à Tecama</p>
                 </a>
 
                 <figure>
-                    <img src="http://placekitten.com/294/176" alt="Imagem Via Placeholder">
+                <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_cmdj_2.webp" alt="Imagem referente a noticia sobre a abertura das inscrições do CMDJ">
                 </figure>
-                <a class="not_link" href="#">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a class="not_link text-start" href="#">
+                    <p>São Roque abre inscrições para eleição do Conselho Municipal dos Direito da Juventude</p>
                 </a>
             </section>
         </div>
--- noticias.php
+++ noticias.php
@@ -32,34 +32,34 @@
         </div>
             <div class="row">
                 <h2>Notícias</h2>
-                <section class="col-lg-8 col-md-8 col-sm-12">
+                <section class="col-lg-7 col-md-7 col-sm-12 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure>
-                    <a href="../print-2-capybaras/noticia2.php"><img src="http://placekitten.com/637/336" alt="Imagem Via Placeholder"></a>
+                    <a href="../print-2-capybaras/noticia2.php"><img src="../print-2-capybaras/imgs/turismo/imgs_not/not_agro.webp" alt="Imagem referente ao evento e noticia agroecologico"></a>
                     </figure>
                     <p><b>Ministro Paulo Teixeira visita São Roque em encontro agroecológico</b></p>
                     <p>O Ministro do Desenvolvimento Agrário e Agricultura Familiar do Brasil, Paulo Teixeira, visitou São Roque na última semana para a mais recente edição do Encontro Paulista de Agroecologia...</p>
                 </section>
 
-                <section class="col-lg-4 col-md-4 col-sm-12">
+                <section class="col-lg-3 col-md-3 col-sm-12 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <div>
                         <figure>
-                            <img src="http://placekitten.com/300/102" alt="Imagem Via Placeholder">
+                        <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_cancer.webp" alt="Imagem referente a noticia da prevenção do cancer de prostata">
                         </figure>
                         <p class="not_titulos">Prefeitura promove palestra de Conscientização e Prevenção do Câncer de Próstata</p>
                     </div>
 
                     <div>
                         <figure>
-                            <img src="http://placekitten.com/300/102" alt="Imagem Via Placeholder">
+                        <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_jose.webp" alt="Imagem referente a noticia do festival José Cabinda">
                         </figure>
                         <p class="not_titulos">Festival José Cabinda acontece neste domingo e segunda-feira</p>
                     </div>
 
                     <div>
                         <figure>
-                            <img src="http://placekitten.com/300/102" alt="Imagem Via Placeholder">
+                        <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_imoveis.webp" alt="Imagem referente a noticia sobre abertura da anistia para regularização de imóveis">
                         </figure>
                         <p class="not_titulos">Prefeitura abre anistia para regularização de imóveis</p>
                     </div>
@@ -68,39 +68,39 @@
             </div>
         </aside>
 
-        <aside class="tur_back text-center">
+        <aside class="not_back text-center">
             <div class="row">
                 <h2>Cidade</h2>
-                <section class="col-lg-6 col-md-6 col-sm-6">
+                <section class="col-lg-5 col-md-5 col-sm-5 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure>
-                        <img src="http://placekitten.com/472/262" alt="Imagem Via Placeholder">
+                    <img class="img-fluid img-thumbnail rounded" src="../print-2-capybaras/imgs/turismo/imgs_not/not_qualifica.webp" alt="Imagem referente a noticia da abertura de inscrições da qualifica são roque">
                     </figure>
                     <p class="not_titulos">Qualifica São Roque abre inscrições para formação de bartender e estratégia de negociação</p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-6">
+                <section class="col-lg-5 col-md-5 col-sm-5 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure>
-                        <img src="http://placekitten.com/472/262" alt="Imagem Via Placeholder">
+                    <img class="img-fluid img-thumbnail rounded" src="../print-2-capybaras/imgs/turismo/imgs_not/not_fanfarra.webp" alt="Imagem referente a noticia do festival municipal de sao roque uma triunfante da fanfarra venceu">
                     </figure>
                     <p class="not_titulos">Triunfante da Fanfarra Municipal de São Roque vence Circuito Paulista de Bandas e Fanfarras</p>
                 </section>
             </div>
 
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-6">
+            <section class="col-lg-5 col-md-5 col-sm-5 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure>
-                        <img src="http://placekitten.com/472/262" alt="Imagem Via Placeholder">
+                    <img class="img-fluid img-thumbnail rounded" src="../print-2-capybaras/imgs/turismo/imgs_not/not_zeladoria.webp" alt="Imagem referente a noticia das ações da zeladoria">
                     </figure>
                     <p class="not_titulos">Confira as ações de zeladoria realizadas pela Prefeitura nesta segunda-feira - 13 de novembro</p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-6">
+                <section class="col-lg-5 col-md-5 col-sm-5 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure>
-                        <img src="http://placekitten.com/472/262" alt="Imagem Via Placeholder">
+                    <img class="img-fluid img-thumbnail rounded" src="../print-2-capybaras/imgs/turismo/imgs_not/not_cmdj.webp" alt="Imagem referente a noticia da abertura das inscrições para eleição do CMDJ">
                     </figure>
                     <p class="not_titulos">São Roque abre inscrições para eleição do Conselho Municipal dos Direito da Juventude</p>
                 </section>
@@ -110,19 +110,19 @@
         <aside>
             <h2>Saúde</h2>
             <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-6">
+            <section class="col-lg-5 col-md-5 col-sm-5 not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure class="text-center">
-                        <img src="http://placekitten.com/612/439" alt="Imagem Via Placeholder">
+                    <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_rotatoria.webp" alt="Imagem referente a noticia da rotatoria fixa sendo construida">
                     </figure>
                     <p><b>Rotatória fixa será construída na região próxima à Tecama</b></p>
                     <p>O trânsito municipal no antigo trecho da Rodovia Raposo Tavares, próximo à fábrica Tecama, terá uma novidade: a implantação de uma rotatória fixa.</p>
                 </section>
 
-                <section class="col-lg-6 col-md-6 col-sm-6 text-end">
+                <section class="col-lg-5 col-md-5 col-sm-5 text-start not_i_shadow">
                     <h3 class="hide">h3 - Section</h3>
                     <figure class="text-center">
-                        <img src="http://placekitten.com/417/439" alt="Imagem Via Placeholder">
+                    <img src="../print-2-capybaras/imgs/turismo/imgs_not/not_circo.webp" alt="Imagem referente a noticia do festival da magia do circo em são roque">
                     </figure>
                     <p><b>Ginástica Rítmica encerra ano de atividades com apresentação A Magia do Circo</b></p>
                     <p>A equipe de Ginástica Rítmica de São Roque realizará nesta semana uma apresentação especial como forma de celebrar um ano de atividades repleto de conquistas.</p>
